7ec00b6c741733d709252ea1aa50dfb6068e7e30
potat0chip <79907738+potat0chip@users.noreply.github.com>
2021-08-09 10:21:13 +0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -7,17 +7,16 @@ from django.contrib.auth import authenticate
 
 def index(request):
     if request.method == "POST":
-        # room_name = "hello"
         form = UsernameForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            nickname = form.cleaned_data['nickname']
             user = authenticate(username=username, password=password)
             if user is not None:
-                # return HttpResponse("Hello, " + username)
                 context = {
                     'username': username,
-                    # 'room_name': room_name,
+                    'nickname': nickname,
                 }
                 return render(request, 'hello/index.html', context)
             else:
