ca0772d4985bc6f2122984f82f877b5aaeeec8ab
potat0chip <79907738+potat0chip@users.noreply.github.com>
2021-08-09 10:19:40 +0500

    Update forms.py

--- hello/forms.py
+++ hello/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class UsernameForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)
+    nickname = forms.CharField(label='nickname', max_length=100)
